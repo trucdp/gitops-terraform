40a43c578c24cee23118c91fb561a763402d2658
Truc Pham <72937605+trucdp@users.noreply.github.com>
2023-10-04 06:32:14 +0000

    push

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.4"
 
   required_providers {
     aws = {
@@ -8,38 +8,7 @@ terraform {
     }
   }
 }
+
 provider "aws" {
   region = local.region
-}
-
-data "aws_availability_zones" "available" {}
-
-locals {
-  name   = "${basename(path.cwd)}-vpc"
-  region = "us-east-1"
-
-  vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
-
-  tags = {
-    Example    = local.name
-    GithubRepo = "terraform-aws-vpc"
-    GithubOrg  = "terraform-aws-modules"
-  }
-}
-
-################################################################################
-# VPC Module
-################################################################################
-
-module "vpc" {
-  source = "../../"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
-
-  tags = local.tags
 }
--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -1,9 +1,31 @@
-module "submodule_cicd" {
-  source = "./cicd"
-  count  = terraform.workspace == local.workspace_cicd ? 1 : 0
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = "${basename(path.cwd)}-vpc"
+  region = "us-east-1"
+
+  vpc_cidr = "10.0.96.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-vpc"
+    GithubOrg  = "terraform-aws-modules"
+  }
 }
 
-module "submodule_qta" {
-  source = "./qta"
-  count  = terraform.workspace == local.workspace_qta ? 1 : 0
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+
+  tags = local.tags
 }

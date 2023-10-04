33995be45e7aaa49278cb31f343ab7d0538cd5f4
Truc Pham <72937605+trucdp@users.noreply.github.com>
2023-10-04 13:27:16 +0700

    Update provider.tf

--- environments/dev/provider.tf
+++ environments/dev/provider.tf
@@ -1,17 +1,45 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
-    tencentcloud = {
-      source  = "tencentcloudstack/tencentcloud"
-      version = "1.77.8"
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
     }
   }
-  backend "cos" {
-    region = "ap-guangzhou"
-    bucket = "keep-gitops-dev-1308919341"
-    prefix = "terraform/state"
+}
+provider "aws" {
+  region = local.region
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = "${basename(path.cwd)}-vpc"
+  region = "us-east-1"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-vpc"
+    GithubOrg  = "terraform-aws-modules"
   }
 }
 
-provider "tencentcloud" {
-  region = "ap-guangzhou"
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  source = "../../"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 4, k)]
+
+  tags = local.tags
 }

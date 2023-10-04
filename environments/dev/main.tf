58a6daf84a524f82abace7e01f1873883e354fd7
Truc Pham <72937605+trucdp@users.noreply.github.com>
2023-10-04 13:43:29 +0700

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -4,7 +4,7 @@ locals {
   name   = "${basename(path.cwd)}-vpc"
   region = "us-east-1"
 
-  vpc_cidr = "10.0.69.0/16"
+  vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
   tags = {
